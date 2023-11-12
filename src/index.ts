72d85944e63852a0858b338ffe791d7592bea275
Dj Isaac <contact@dejaydev.com>
2023-11-11 23:15:53 -0600

    more scaffolding

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
-app.get('/', (c) => c.text('Hello Hono!'))
+const app = new Hono();
+app.get("/", (c) => c.text("Hello Hono!"));
 
-serve(app)
+// ---
+serve(app, (addr) => {
+  console.log(`Adapto vXYZ, bound to http://[${addr.address}]:${addr.port}`);
+});
